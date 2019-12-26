7c8e680729a217670b7c8993213d7ca6580c9544
gdepuydt <geert_depuydt@hotmail.com>
2019-12-26 11:23:23 +0100

    02:02:00

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,21 @@ const MAX_RESIZERS: usize = (1 << (32 - RESIZE_STAMP_BITS)) -1;
 const RESIZE_STAMP_SHIFT: usize = 32 - RESIZE_STAMP_BITS;
 
 mod node;
+
+use crossbeam::epoch::{Atomic, Shared, Guard};
+use std::sync::atomic::Ordering;
+
+pub struct FlurryHashMap<K, V> {
+    table: Atomic<Table<K, V>>, 
+    
+}
+
+struct Table<K, V> {
+    bins: [Atomic<node::BinEntry<K, V>>],
+}
+
+impl Table<K, V> {
+    fn at<'g>(&'g self, i: usize, guard: &'g Guard) -> Shared<'g, node::BinEntry<K, V>> {
+        self.bins[i].load(Ordering::Acquire, guard)
+    }
+}
--- src/node.rs
+++ src/node.rs
@@ -1,5 +1,6 @@
 use std::cell::UnsafeCell;
-use crossbeam::epoch::Atomic;
+use crossbeam::epoch::{Atomic, Guard, Shared};
+use std::sync::atomic::Ordering;
 
 pub(crate) enum BinEntry<K, V> {
     Node(Node<K, V>),
@@ -9,12 +10,17 @@ impl<K, V> BinEntry<K, V>
 where 
     K: Eq, 
 {
-    pub(crate) fn find(&self, hash: u64, key: &K) -> Option<&Node<K, V>> {
+    pub(crate) fn find<'g>(&'g self, hash: u64, key: &K, guard: &'g Guard) -> Shared<'g, Node<K, V>> {
         match *self {
-            BinEntry::Node(ref start) => {
-                // TODO
-
+            BinEntry::Node(ref n) => {
+                if n.hash == hash && &n.key == key {
+                    return Shared::from(n as *const _);
                 }
+                let next = n.next.load(Ordering::SeqCst, guard);
+                if next.is_null() {
+                    return Shared::null();
+                }
+                return next;
             }
         }
     }
@@ -24,5 +30,5 @@ pub(crate) struct Node<K, V> {
     pub(crate) hash: u64,
     pub(crate) key: K,
     pub(crate) value: UnsafeCell<V>,
-    pub(crate) next: Atomic<BinEntry<K, V>>,
+    pub(crate) next: Atomic<Node<K, V>>,
 }
